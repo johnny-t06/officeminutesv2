775aac6da914e53b312eb3cf02b9df0fc3fba637
johnny-t06 <jonnytan0818@gmail.com>
2024-08-05 14:10:27 -0700

    context and type changes

---
+++ src/app/context/UserSessionContext.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { IdentifiableUser } from "@interfaces/type";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import React from "react";
+import { auth } from "@project/firebase";
+import { addUser, getUser } from "@services/client/user";
+import { useRouter } from "next/navigation";
+
+interface Session {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  error: string | null;
+}
+
+interface IUserSessionContext {
+  user: IdentifiableUser | null;
+  session: Session;
+  onSignIn: () => Promise<void>;
+  onSignOut: () => Promise<void>;
+}
+
+const UserSessionContext = React.createContext<IUserSessionContext>(
+  {} as IUserSessionContext
+);
+
+export const UserSessionContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [user, setUser] = React.useState<IdentifiableUser | null>(null);
+  const [session, setSession] = React.useState<Session>({
+    isAuthenticated: false,
+    isLoading: true,
+    error: null,
+  });
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (firebaseUser) {
+        const currUser = await getUser(firebaseUser?.uid);
+        setUser(currUser);
+        setSession({ isAuthenticated: true, isLoading: false, error: null });
+        router.push("/course");
+      } else {
+        setUser(null);
+        setSession({ isAuthenticated: false, isLoading: false, error: null });
+        router.push("/");
+      }
+    });
+
+    return () => unsub();
+  }, []);
+
+  const onSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      if ((await getUser(user.uid)) == null) {
+        await addUser({
+          id: user.uid,
+          name: user.displayName ?? "",
+          tufts_username: "",
+          email: user.email ?? "",
+          role: "user",
+        });
+      }
+
+      setSession((prev) => ({ ...prev, isLoading: true }));
+    } catch (e: any) {
+      setSession({
+        isAuthenticated: false,
+        isLoading: false,
+        error: e.message,
+      });
+    }
+  };
+
+  const onSignOut = async () => {
+    try {
+      setSession((prev) => ({ ...prev, isLoading: true }));
+      signOut(auth);
+      setSession({ isAuthenticated: false, isLoading: false, error: null });
+    } catch (e: any) {
+      setSession({
+        isAuthenticated: false,
+        isLoading: false,
+        error: e.message,
+      });
+    }
+  };
+  return (
+    <UserSessionContext.Provider value={{ user, session, onSignIn, onSignOut }}>
+      {children}
+    </UserSessionContext.Provider>
+  );
+};
+
+export const useUserSession = () => React.useContext(UserSessionContext);
