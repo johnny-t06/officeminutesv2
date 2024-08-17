dbc90f49b7b7191fabbfbd584625276f96897711
nickbar01234 <nickbar01234@gmail.com>
2024-08-17 10:55:33 -0400

    Fix hover color

--- src/app/private/course/[courseId]/board/page.tsx
+++ src/app/private/course/[courseId]/board/page.tsx
@@ -2,7 +2,14 @@
 import Board from "@components/board";
 import Header from "@components/Header";
 import { useOfficeHour } from "@hooks/oh/useOfficeHour";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import CheckIcon from "@mui/icons-material/Check";
 
 import React from "react";
@@ -33,17 +40,21 @@ const Page = (props: PageProps) => {
     }
   };
 
-  const addOrRemoveTopic = (topic: string) => {
-    setSelectedTopics((topics) => {
-      if (topics.includes(topic)) {
-        return topics.filter((other) => other !== topic);
-      } else if (topic === SELECT_ALL) {
+  const addOrRemoveTopic = (
+    _e: React.MouseEvent<HTMLElement>,
+    newTopics: string[]
+  ) => {
+    setSelectedTopics((currentSelectedTopics) => {
+      if (currentSelectedTopics.includes(SELECT_ALL)) {
+        return newTopics.filter((topic) => topic !== SELECT_ALL);
+      } else if (newTopics.includes(SELECT_ALL)) {
         return [SELECT_ALL];
       } else {
-        return [...topics.filter((other) => other !== SELECT_ALL), topic];
+        return newTopics;
       }
     });
   };
+
   return (
     <Box>
       <Header
@@ -59,39 +70,63 @@ const Page = (props: PageProps) => {
           </Button>
         }
       />
-      <Box display="flex" columnGap="16px" overflow="auto" paddingBottom="16px">
-        {topics.map((topic) => (
-          <Button
-            key={topic}
-            sx={{
-              flexShrink: 0,
-              padding: "4px 12px",
-              color: "#1D192B",
-              whiteSpace: "nowrap",
-              minWidth: "content",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: selectedTopics.includes(topic) ? "#D7E3F8" : "",
-              columnGap: "8px",
-              textTransform: "none",
-              border: selectedTopics.includes(topic) ? 0 : 1,
-              borderColor: "#73777F",
-              borderRadius: "10px",
-            }}
-            onClick={() => addOrRemoveTopic(topic)}
-          >
-            <CheckIcon
+      <ToggleButtonGroup value={selectedTopics} onChange={addOrRemoveTopic}>
+        <Stack
+          spacing="16px"
+          direction="row"
+          overflow="auto"
+          paddingBottom="16px"
+          marginBottom="16px"
+          maxWidth="100vw"
+        >
+          {topics.map((topic) => (
+            <ToggleButton
+              id={topic}
+              key={topic}
+              value={topic}
+              aria-label={topic}
+              selected={selectedTopics.includes(topic)}
               sx={{
-                display: selectedTopics.includes(topic) ? "block" : "none",
+                "&.Mui-selected": {
+                  backgroundColor: "#D7E3F8",
+                  border: 0,
+                },
+                "@media (hover: none)": {
+                  "&:hover": {
+                    backgroundColor: `${
+                      selectedTopics.includes(topic) ? "#D7E3F8" : "#FFF"
+                    } !important`,
+                  },
+                },
               }}
-            />
-            <label style={{ display: "block", fontWeight: "bold" }}>
-              {topic}
-            </label>
-          </Button>
-        ))}
-      </Box>
+            >
+              <Box
+                padding="4px 12px"
+                color="#1D192B"
+                whiteSpace="nowrap"
+                minWidth="content"
+                borderRadius="10px"
+                textTransform="none"
+                alignItems="center"
+                justifyContent="center"
+                columnGap="8px"
+                borderColor="#73777F"
+                display="flex"
+                overflow="hidden"
+              >
+                <CheckIcon
+                  sx={{
+                    display: selectedTopics.includes(topic) ? "block" : "none",
+                  }}
+                />
+                <label style={{ display: "block", fontWeight: "bold" }}>
+                  {topic}
+                </label>
+              </Box>
+            </ToggleButton>
+          ))}
+        </Stack>
+      </ToggleButtonGroup>
       <Board questions={getQuestionsByTopic(selectedTopics)} />
     </Box>
   );
