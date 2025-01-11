1b9054f7a04a805dd0689eab578e77aeb292fc42
johnny-t06 <jonnytan0818@gmail.com>
2025-01-10 23:38:55 -0800

    Throw if firebase env is not set

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,9 +1,12 @@
 import * as admin from "firebase-admin";
+if (!process.env.FIREBASE_ADMIN_SDK_JSON) {
+  throw new Error("FIREBASE_ADMIN_SDK_JSON is not set");
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(
-      JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON || "{}")
+      JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON)
     ),
   });
 }
