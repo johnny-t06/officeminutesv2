f7a03a81f8cb571fb76c731e12282bc1640a075e
Loc Mai <locmai0315@gmail.com>
2025-01-25 17:44:15 -0500

    ui: board menu/title + style cleanup

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,21 +9,38 @@ interface IHeader {
 }
 
 const Header = (props: IHeader) => {
-  const { title, leftIcon, rightIcon, alignCenter } = props;
+  const { title, leftIcon, rightIcon } = props;
+
   return (
     <AppBar position="static" elevation={0} color="inherit">
-      <Toolbar disableGutters sx={{ marginLeft: "1%" }}>
-        {leftIcon}
-        <Typography
-          variant="h6"
-          component="div"
-          className={`flex-grow ${alignCenter ? "text-center" : "text-start"}`}
-          sx={{ fontWeight: 400, marginLeft: alignCenter ? "0" : "5%" }}
+      <Toolbar
+        disableGutters
+        sx={{
+          marginX: "1%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          width="100%"
         >
-          {title}
-        </Typography>
+          {leftIcon}
+          {rightIcon ? (
+            <Box display="flex" alignItems="center">
+              <Typography variant="h6" textAlign="left">
+                {title}
+              </Typography>
+            </Box>
+          ) : (
+            <Typography variant="h6" textAlign="center" flexGrow={1}>
+              {title}
+            </Typography>
+          )}
+        </Box>
 
-        {alignCenter && !rightIcon ? <Box className="w-[40px]" /> : rightIcon}
+        {!rightIcon ? <Box width="32px" /> : rightIcon}
       </Toolbar>
     </AppBar>
   );
