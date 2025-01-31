7e191c93a97c515a58d2490267aa00fa1d4537e3
wkim10 <wkim10@tufts.edu>
2025-01-31 11:55:02 -0500

    initial animation test

--- src/app/components/board/QuestionDetails.tsx
+++ src/app/components/board/QuestionDetails.tsx
@@ -147,9 +147,21 @@ export const QuestionDetails = (props: QuestionDetailsProps) => {
 
   return (
     <Box>
-      {loading ? (
-        <div className="flex items-center justify-center h-screen ">
-          <Spinner />
+      {!loading ? (
+        <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+          {/* <Spinner /> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="5em"
+            height="5em"
+            viewBox="0 0 24 24"
+            className="animate-[spin_2s_linear_infinite]"
+          >
+            <path
+              fill="currentColor"
+              d="M15 4a8 8 0 0 1 8 8a8 8 0 0 1-8 8a8 8 0 0 1-8-8a8 8 0 0 1 8-8m0 2a6 6 0 0 0-6 6a6 6 0 0 0 6 6a6 6 0 0 0 6-6a6 6 0 0 0-6-6m-1 2h1.5v3.78l2.33 2.33l-1.06 1.06L14 12.4zM2 18a1 1 0 0 1-1-1a1 1 0 0 1 1-1h3.83c.31.71.71 1.38 1.17 2zm1-5a1 1 0 0 1-1-1a1 1 0 0 1 1-1h2.05L5 12l.05 1zm1-5a1 1 0 0 1-1-1a1 1 0 0 1 1-1h3c-.46.62-.86 1.29-1.17 2z"
+            />
+          </svg>
         </div>
       ) : (
         <>
