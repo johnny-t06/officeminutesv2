a4b7b0235fa979203af5400a0f09d216cbce43c7
Loc Mai <locmai0315@gmail.com>
2025-02-03 14:58:00 -0500

    refactor: delete unnecessary files

--- src/app/private/course/[courseId]/board/[questionId]/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import ErrorNavigation from "@components/ErrorNavigation";
-import React from "react";
-
-const Error = () => {
-  return <ErrorNavigation />;
-};
-
-export default Error;
