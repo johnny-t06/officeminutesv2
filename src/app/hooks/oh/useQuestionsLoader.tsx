a80bb08c134ee18ee22a16778ea63e2327fce3e1
Johnny Tan <jonnytan0818@gmail.com>
2025-02-04 01:27:45 -0500

    Query changes

--- src/app/hooks/oh/useQuestionsLoader.tsx
+++ src/app/hooks/oh/useQuestionsLoader.tsx
@@ -1,12 +1,13 @@
 import { State, useLoadingValue } from "@hooks/utils/useLoadingValue";
 import React from "react";
 import { IdentifiableQuestions } from "@interfaces/type";
-import { getQuestions } from "services/client/question";
 import {
   collection,
   onSnapshot,
+  query,
   Timestamp,
   Unsubscribe,
+  where,
 } from "firebase/firestore";
 import { db } from "@project/firebase";
 import { questionConverter } from "@services/firestore";
@@ -28,45 +29,44 @@ export const useQuestionsLoader = (props: UseQuestionsLoaderProps) => {
   const unsubscriber = React.useRef<Unsubscribe | null>(null);
 
   React.useEffect(() => {
-    getQuestions(props.courseId).then((value) => {
-      setValue(value);
-    });
-  }, [props.courseId]);
-
-  React.useEffect(() => {
-    if (state.state !== State.SUCCESS) {
-      if (unsubscriber.current !== null) {
-        unsubscriber.current();
-        unsubscriber.current = null;
-      }
-      return;
+    if (unsubscriber.current !== null) {
+      unsubscriber.current();
+      unsubscriber.current = null;
     }
 
-    const unsubscribe = onSnapshot(
+    const oneDayAgo = Timestamp.fromMillis(Date.now() - 24 * 60 * 60 * 1000);
+
+    const q = query(
       collection(db, `courses/${courseId}/questions`).withConverter(
         questionConverter
       ),
-      (snapshot) => {
-        const docByType = groupDocChangesByType(snapshot.docChanges());
-        const removed = docByType["removed"] ?? [];
-        const modified = docByType["modified"] ?? [];
-        const added = docByType["added"] ?? [];
-        setValue((prev) => {
-          const updated = prev
-            .filter((doc) => !removed.some((d) => d.id === doc.id))
-            .map((doc) => {
-              return modified.find((d) => d.id === doc.id) ?? doc;
-            });
-          return [
-            ...updated,
-            ...added.map((doc) => ({
-              ...doc,
-              timestamp: doc.timestamp ?? Timestamp.now(),
-            })),
-          ];
-        });
-      }
+      where("state", "!=", 3),
+      where("timestamp", ">", oneDayAgo)
     );
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const docByType = groupDocChangesByType(snapshot.docChanges());
+      const removed = docByType["removed"] ?? [];
+      const modified = docByType["modified"] ?? [];
+      const added = docByType["added"] ?? [];
+      console.log("changes", docByType);
+      setValue((prev) => {
+        const updated = prev
+          .filter((doc) => !removed.some((d) => d.id === doc.id))
+          .map((doc) => {
+            return modified.find((d) => d.id === doc.id) ?? doc;
+          });
+        const addedDocs = added
+          .filter((doc) => !prev.some((prevDoc) => prevDoc.id === doc.id))
+          .map((doc) => ({
+            ...doc,
+            timestamp: doc.timestamp ?? Timestamp.now(),
+          }));
+        console.log("updated", updated);
+        console.log("added", addedDocs);
+        return [...updated, ...addedDocs];
+      });
+    });
 
     unsubscriber.current = unsubscribe;
 
@@ -76,7 +76,7 @@ export const useQuestionsLoader = (props: UseQuestionsLoaderProps) => {
         unsubscriber.current = null;
       }
     };
-  }, [state.state, courseId]);
+  }, [courseId]);
 
   // TODO(lnguyen2693) - handle setError
   return { questions: state };
