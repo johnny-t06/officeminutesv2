94154fb4f33e7aa864c0e951775bfe309193aa1d
Loc Mai <91977124+locmai1@users.noreply.github.com>
2025-03-03 20:42:30 -0500

    style: new queue view

--- src/app/components/queue/EditQuestion.tsx
+++ src/app/components/queue/EditQuestion.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { Box, Button, Container, Typography } from "@mui/material";
-import NotificationAddOutlinedIcon from "@mui/icons-material/NotificationAddOutlined";
-import KeyboardReturnOutlinedIcon from "@mui/icons-material/KeyboardReturnOutlined";
+
 import { useOfficeHour } from "@hooks/oh/useOfficeHour";
 import React from "react";
 import { CustomModal } from "@components/CustomModal";
 import { leaveQuestionGroup } from "@services/client/question";
-import QuestionForm from "./form/QuestionForm";
 import { useUserOrRedirect } from "@hooks/useUserOrRedirect";
 import { IdentifiableQuestion, IdentifiableUsers } from "@interfaces/type";
 import { getUsers } from "@services/client/user";
@@ -14,6 +11,7 @@ import { useLoading } from "@context/LoadingContext";
 import { QuestionState } from "@interfaces/db";
 import { StudentHelping } from "./StudentHelping";
 import { StudentMissing } from "./StudentMissing";
+import { EditQuestionItem } from "./EditQuestionItem";
 
 interface EditQuestionProps {
   queuePos: number;
@@ -64,148 +62,61 @@ export const EditQuestion = (props: EditQuestionProps) => {
     },
   ];
 
-  const editButton = (
-    <Button
-      sx={{
-        fontWeight: 500,
-        fontSize: 14,
-        textTransform: "initial",
-        borderRadius: "100px",
-        flexGrow: 1,
-      }}
-      fullWidth
-      variant="outlined"
-    >
-      Edit submission
-    </Button>
-  );
-
   // not in queue, not join any question,
   // and not have any IN_PROGRESS or MISSING question
   if (queuePos === -1 && groupPos === -1 && currQuestion.title === "") {
     return null;
   }
 
-  const position =
-    queuePos === -1
-      ? groupPos
-      : groupPos === -1
-      ? queuePos
-      : Math.min(queuePos, groupPos);
-
-  const isAuthor = position === queuePos;
-
   if (currQuestion.state === QuestionState.IN_PROGRESS) {
     const ta = tas.find((myTa) => myTa.id === currQuestion.helpedBy);
-
-    if (!ta) {
-      return null;
-    }
-
-    return <StudentHelping currQuestion={currQuestion} ta={ta} />;
+    return ta ? <StudentHelping currQuestion={currQuestion} ta={ta} /> : null;
   }
+
   if (currQuestion.state === QuestionState.MISSING) {
     return <StudentMissing currQuestion={currQuestion} />;
   }
 
+  const questionsToDisplay = [];
+
+  if (queuePos !== -1) {
+    questionsToDisplay.push({
+      position: queuePos,
+      question: currQuestion,
+      leaveQueue: () => setLeaveQueueModal(true),
+    });
+  }
+
+  if (groupPos !== -1) {
+    questionsToDisplay.push({
+      position: groupPos,
+      question: groupQuestion,
+      leaveQueue: leaveGroup,
+    });
+  }
+
+  questionsToDisplay.sort((a, b) => a.position - b.position);
+
   return (
-    <>
-      {isAuthor && (
+    <React.Fragment>
+      {!currQuestion.questionPublic && (
         <CustomModal
           title="Leave queue?"
-          subtitle="You'll lose your place in line and
-                  won't receive assistance until you join again."
+          subtitle="You'll lose your place in line and won't receive assistance until you join again."
           buttons={leaveQueueButtons}
           open={leaveQueueModal}
           setOpen={setLeaveQueueModal}
         />
       )}
 
-      <Container
-        sx={{
-          bgcolor: "primary.light",
-          padding: "16px",
-          borderRadius: "12px",
-          display: "flex",
-          flexDirection: "column",
-          rowGap: "12px",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          sx={{ fontWeight: 400, fontSize: "16px", color: "#545F70" }}
-        >
-          Your queue position
-        </Typography>
-        <Typography
-          sx={{ fontWeight: 700, fontSize: "45px", textAlign: "center" }}
-        >
-          {position === 0 ? "You're next!" : position + 1}
-        </Typography>
-
-        <Box
-          sx={{
-            fontWeight: 400,
-            fontSize: 12,
-            display: "flex",
-            flexDirection: "row",
-            gap: 1,
-            color: "#545F70",
-            alignItems: "center",
-          }}
-        >
-          <NotificationAddOutlinedIcon style={{ fontSize: "16px" }} />
-          <Typography sx={{ fontSize: "16px" }}>
-            We'll notify you when it's your turn
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: 1,
-            columnGap: "8px",
-            fontWeight: 500,
-            fontSize: "14px",
-            marginLeft: 2,
-            marginRight: 2,
-          }}
-        >
-          <Box sx={{ flexGrow: 1 }}>
-            <Button
-              variant="contained"
-              sx={{
-                fontWeight: 500,
-                fontSize: 14,
-                textTransform: "initial",
-                borderRadius: "100px",
-                display: "flex",
-                flexDirection: "row",
-                columnGap: 1,
-              }}
-              style={{ boxShadow: "none" }}
-              fullWidth
-              onClick={() =>
-                isAuthor ? setLeaveQueueModal(true) : leaveGroup()
-              }
-            >
-              <KeyboardReturnOutlinedIcon style={{ fontSize: "14px" }} />
-              {isAuthor ? "Leave queue " : "Leave group"}
-            </Button>
-          </Box>
-          {isAuthor && (
-            <Box sx={{ flexGrow: 1 }}>
-              <QuestionForm
-                triggerButton={editButton}
-                title="Edit submission"
-                currentQuestion={currQuestion}
-              />
-            </Box>
-          )}
-        </Box>
-      </Container>
-    </>
+      {questionsToDisplay.map((item, index) => (
+        <EditQuestionItem
+          key={index}
+          position={item.position}
+          question={item.question}
+          leaveQueue={item.leaveQueue}
+        />
+      ))}
+    </React.Fragment>
   );
 };
---
+++ src/app/components/queue/EditQuestionItem.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  Divider,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from "@mui/material";
+import KeyboardReturnOutlinedIcon from "@mui/icons-material/KeyboardReturnOutlined";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import React from "react";
+import { IdentifiableQuestion, IdentifiableUsers } from "@interfaces/type";
+import { getUsers } from "@services/client/user";
+import { trimUserName } from "@utils/index";
+
+interface EditQuestionItemProps {
+  position: number;
+  question: IdentifiableQuestion;
+  leaveQueue: (modal?: boolean) => void;
+}
+
+export const EditQuestionItem = (props: EditQuestionItemProps) => {
+  const { position, question, leaveQueue } = props;
+  const [users, setUsers] = React.useState<IdentifiableUsers>([]);
+  const isPrivate = !question.questionPublic;
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const fetchedUsers = await getUsers(question.group);
+      setUsers(fetchedUsers);
+    };
+    fetchUsers();
+  }, [question.group]);
+
+  return (
+    <Container
+      sx={{
+        bgcolor: "primary.light",
+        paddingY: "16px",
+        borderRadius: "12px",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "12px",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          rowGap: "10px",
+          padding: 0,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            width: 1,
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: 400,
+              fontSize: "12px",
+              color: "#545F70",
+              marginTop: "10px",
+            }}
+          >
+            {isPrivate ? "Private question" : "Your queue position"}
+          </Typography>
+          <Typography
+            sx={{ fontWeight: 700, fontSize: "45px", textAlign: "center" }}
+          >
+            {position + 1}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            rowGap: "10px",
+            width: 1,
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              fontWeight: 500,
+              fontSize: 12,
+              textTransform: "initial",
+              borderRadius: "100px",
+              display: "flex",
+              flexDirection: "row",
+              boxShadow: "none",
+            }}
+            fullWidth
+            onClick={() => leaveQueue(true)}
+            aria-label={isPrivate ? "Leave queue" : "Leave group"}
+          >
+            <KeyboardReturnOutlinedIcon style={{ fontSize: "14px" }} />
+            {isPrivate ? "Leave queue " : "Leave group"}
+          </Button>
+          <Button
+            sx={{
+              fontWeight: 500,
+              fontSize: 12,
+              textTransform: "initial",
+              borderRadius: "100px",
+            }}
+            fullWidth
+            variant="outlined"
+            aria-label="Add to submission"
+          >
+            Add to submission
+          </Button>
+        </Box>
+      </Container>
+      <Divider flexItem />
+      <Accordion
+        disableGutters
+        sx={{
+          width: 1,
+          boxShadow: "none",
+          paddingX: "4px",
+          backgroundColor: "transparent",
+          "&.MuiAccordion-root": {
+            "&:before": {
+              display: "none",
+            },
+          },
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<KeyboardArrowUpIcon sx={{ color: "#38608F" }} />}
+          sx={{
+            padding: 0,
+            marginBottom: 0,
+          }}
+          aria-controls={`question-${question.id}-content`}
+          id={`question-${question.id}-header`}
+        >
+          <Typography sx={{ fontWeight: 500, fontSize: "16px" }}>
+            {question.title}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails
+          sx={{ padding: "0 0 8px 0" }}
+          id={`question-${question.id}-content`}
+          aria-labelledby={`question-${question.id}-header`}
+        >
+          <Typography sx={{ fontSize: 14, color: "#43474E" }}>
+            {users.map(trimUserName).join(", ")}
+          </Typography>
+          <Typography
+            sx={{ fontSize: "14px", color: "#43474E", marginTop: "10px" }}
+          >
+            {question.description}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Container>
+  );
+};
